7c2ecab8f316e85b8fee010bc8901e93d4436018
Anett <sz.anett888@gmail.com>
2021-03-17 13:46:24 +0100

    AlapszerkezetJs

---
+++ public_html/JS.js
@@ -0,0 +1,38 @@
+var tomb = [];
+function feltolt(db) {
+    tomb=[];
+    for (var i = 0; i < db; i++) {
+        var vel = Math.floor(Math.random() * 10 + 1);
+        tomb[i] = vel;
+
+    }
+    console.log(tomb);
+    document.getElementById("szoveg").innerHTML = "A tömb elemei: " + tomb;
+    
+   
+}
+
+function osszegzes(){
+    var osszeg=0;
+    for (var i = 0; i < tomb.length; i++) {
+        osszeg+=tomb[i];
+    }
+    document.getElementById("osszeg").innerHTML = "Az elemek összege: " + osszeg;
+}
+
+function dolgozz() {
+    
+    var adat = document.getElementById("adat").value;
+    feltolt(adat);
+    osszegzes();
+
+}
+
+function init() {
+    console.log("Itt vagyok");
+    document.getElementById("szoveg").innerHTML = "A Js dolgozik!";
+    document.getElementById("ok").addEventListener("click", dolgozz);
+}
+
+window.addEventListener("load", init)
+
